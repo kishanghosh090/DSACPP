4adcd5572f1f59c6f7291aabb731c68b58a334f3
kishanghosh090 <kishanghosh090@gmail.com>
2025-06-11 10:30:42 +0530

    doubly linked list

---
+++ linkedList/doublyLinkedList.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+
+// create Node------
+class Node
+{
+
+public:
+    int data;
+    Node *prev;
+    Node *next;
+    Node()
+    {
+        prev = NULL;
+        next = next;
+    }
+};
+
+// create Doubly Linked List-----
+
+class Dll
+{
+    Node *head = NULL;
+
+public:
+    void print()
+    {
+        if (head == NULL)
+        {
+            cout << "List is empty";
+        }
+        else
+        {
+            Node *temp = head;
+            while (temp->next != NULL)
+            {
+                cout << temp->data << "->";
+                temp = temp->next;
+                if (temp->next == NULL)
+                {
+                    cout << temp->data << "->";
+                }
+            }
+        }
+        cout << "\n";
+    }
+    void insert_at_end(int value)
+    {
+        Node *temp = new Node();
+        temp->data = value;
+        Node *t = head;
+
+        if (head == NULL)
+        {
+            head = temp;
+        }
+        else
+        {
+            while (t->next != NULL)
+            {
+                t = t->next;
+            }
+            t->next = temp;
+            temp->prev = t;
+        }
+    }
+};
+int main()
+{
+
+    Dll dll;
+
+    dll.insert_at_end(19);
+    dll.insert_at_end(20);
+    dll.insert_at_end(22);
+    dll.insert_at_end(24);
+    dll.insert_at_end(25);
+    dll.print();
+
+    return 0;
+}
