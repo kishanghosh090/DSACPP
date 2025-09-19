abaaf56a8f2a02b78b038e829e842943a16a33b2
kishanghosh090 <kishanghosh090@gmail.com>
2025-09-20 00:19:05 +0530

    add rivison cpp vars

---
+++ chaiAurCPP/basics/01_variable/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    const string name = "kishan";
+    cout << name << endl;
+
+    cout << "Welcome to chai aur code" << endl;
+
+    const int a = 12;
+
+    return 0;
+}
--- chaiAurCPP/test/hello.cpp
+++ chaiAurCPP/test/hello.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 
+namespace myChai
+{
+    void print(char *arr)
+    {
+        std::cout << arr;
+    }
+}
+
 int main()
 {
-    std::cout << "hello from chai";
+    std::cout << "hello from chai\n";
+    myChai::print("kishan");
+    return 0;
+}
+
+int Main()
+{
     return 0;
 }
