7b9363ff3c9387b51ec1fdf1a772576978a2dd67
kishanghosh090 <kishanghosh090@gmail.com>
2025-09-20 00:48:21 +0530

    add rivison cpp data types

--- chaiAurCPP/basics/02_premitive/data.cpp
+++ chaiAurCPP/basics/02_premitive/data.cpp
@@ -7,5 +7,11 @@ int main()
     float temp = 85.55;
     double price = 199.999;
 
+    string teaName;
+    getline(cin, teaName);
+    int teaQuantity;
+    cin >> teaQuantity;
+    cout << "Your tea " << teaName << ". Tea " << teaQuantity << " is ready.";
+
     return 0;
 }
