7d09ea9008bf494d64282ea5e7cebe98f0bdf8b1
kishanghosh090 <kishanghosh090@gmail.com>
2025-09-19 11:10:58 +0530

    add delete from pos

--- rivison2/linkedList/singly_linkedlist.cpp
+++ rivison2/linkedList/singly_linkedlist.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdio.h>
 using namespace std;
 
 // create node for LL ------------------
@@ -58,23 +59,9 @@ public:
         {
             insert_at_begin(value);
         }
-        // else if (head->next == NULL)
-        // {
-        //     // Node *temp = head;
-        //     // head = NULL;
-        //     // delete (temp);
-        // }
+
         else
         {
-            // Node *temp = head;
-            // while (temp->next->next != NULL)
-            // {
-            //     temp = temp->next;
-            // }
-            // Node *t = temp->next;
-            // temp->next = NULL;
-            // delete (t);
-
             Node *temp = head;
             while (temp->next != NULL)
             {
@@ -135,6 +122,7 @@ public:
             sizeOfLinkedList--;
             return;
         }
+
         Node *temp = head;
         head = temp->next;
         delete (temp);
@@ -142,10 +130,70 @@ public:
     }
 
     // void delete from end---------------
-    void delete_from_end() {}
+    void delete_from_end()
+    {
+        if (head == NULL)
+        {
+            cout << "ouuchhhh!!!...Linked list is empty";
+        }
 
+        else if (head->next == NULL)
+        {
+            Node *temp = head;
+            head = NULL;
+            delete (temp);
+            sizeOfLinkedList--;
+        }
+        else
+        {
+            Node *temp = head;
+            while (temp->next->next != NULL)
+            {
+                temp = temp->next;
+            }
+            Node *t = temp->next;
+            temp->next = NULL;
+            delete (t);
+            sizeOfLinkedList--;
+        }
+    }
     // delete from position---------------
-    void delete_from_position() {}
+    void delete_from_position(int position)
+    {
+
+        if (head == NULL)
+        {
+            cout << "Linked List is empty";
+            return;
+        }
+
+        if (position > sizeOfLinkedList)
+        {
+            cout << "Position which is given by you exided the size of linked list";
+            return;
+        }
+        if (position <= 0)
+        {
+            cout << "position which is given by you is invalid";
+            return;
+        }
+        if (position == 1)
+        {
+            delete_from_begin();
+            return;
+        }
+
+        Node *temp = head;
+        while ((position - 2) > 0 && (temp->next != NULL))
+        {
+            temp = temp->next;
+            position--;
+        }
+        Node *t = temp->next;
+        temp->next = temp->next->next;
+        delete (t);
+        sizeOfLinkedList--;
+    }
 };
 
 int main()
@@ -158,10 +206,12 @@ int main()
     ll1.insert_at_begin(54);
     ll1.insert_at_end(10000);
     ll1.insert_at_position(3, 76);
-    ll1.delete_from_begin();
+    // ll1.delete_from_end();
     // ll1.delete_from_begin();
     // ll1.delete_from_begin();
     // ll1.delete_from_begin();
+    ll1.delete_from_position(3);
+    ll1.delete_from_position(1);
     ll1.print();
     return 0;
 }
