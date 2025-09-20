65d6b5bc1d86333478bf1be1c3305b7cbd25c29e
kishanghosh090 <kishanghosh090@gmail.com>
2025-09-20 23:44:42 +0530

    add cpp revison arr and pointer

--- chaiAurCPP/array/three.cpp
+++ chaiAurCPP/array/three.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
 using namespace std;
 
+int totalChaiServed(int chai[], int size)
+{
+    int total = 0;
+    for (int i = 0; i < size; i++)
+    {
+        total += chai[i];
+    }
+}
+
+int *prepareChaiOrders(int cups)
+{
+    int *orders = new int[cups];
+
+    for (int i = 0; i < cups; i++)
+    {
+        orders[i] = (i + 1) * 10;
+    }
+    return orders;
+}
+
 int main()
 {
-    
+    int chais[5] = {1, 2, 3, 4, 6};
+
+    int cups = 5;
+    int *a = prepareChaiOrders(cups);
+    for (int i = 0; i < cups; i++)
+    {
+        cout << a[i] << endl;
+    }
+    delete[] a;
     return 0;
 }
