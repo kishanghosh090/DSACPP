8331a1f225a8f7ed47729fd21a7aaee4638ef0be
kishanghosh090 <kishanghosh090@gmail.com>
2025-09-21 00:29:01 +0530

    add cpp revison oops

---
+++ chaiAurCPP/oops/myClass.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Chai
+{
+    // data members (attributes)
+private:
+    string teaName;
+    int servings;
+    vector<string> ingredientes;
+
+    // Member function
+public:
+    Chai();
+    ~Chai();
+    void displayChaiDetails()
+    {
+        cout << "Tea Name " << teaName << endl;
+        cout << "Tea servings " << servings << endl;
+        for (string i : ingredientes)
+        {
+            cout << i << endl;
+        }
+    }
+};
+Chai::Chai() {}
+Chai::~Chai() {}
+
+int main()
+{
+    return 0;
+}
