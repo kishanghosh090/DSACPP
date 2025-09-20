bce789f6c70cee3a9b3484d0a3ff37610485a3e6
kishanghosh090 <kishanghosh090@gmail.com>
2025-09-20 23:24:57 +0530

    add cpp revison

---
+++ chaiAurCPP/array/one.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int chaiTemp[5] = {2323, 12, 32, 412, 656};
+    cout << "chai temps";
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "chai temp: " << chaiTemp[i] << endl;
+    }
+
+    return 0;
+}
