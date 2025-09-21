4868dfef7fd65b0ba910971dd7c48a44f958ae4a
kishanghosh090 <kishanghosh090@gmail.com>
2025-09-21 21:29:35 +0530

    add cl insert val  method implemented

--- rivison2/linkedList/cl.cpp
+++ rivison2/linkedList/cl.cpp
@@ -43,6 +43,68 @@ public:
             temp = temp->next;
         }
     }
+
+    void insert_at_begin(int value)
+    {
+        if (head == NULL && tail == NULL)
+        {
+            Node *newNode = new Node();
+            head = newNode;
+            tail = newNode;
+            sizeOfLL++;
+            return;
+        }
+
+        Node *newNode = new Node();
+        newNode->data = value;
+
+        newNode->next = head;
+        head->prev = newNode;
+        head = newNode;
+        newNode->prev = tail;
+        tail->next = newNode;
+        sizeOfLL++;
+    }
+    void insert_at_end(int value)
+    {
+        if (head == NULL && tail == NULL)
+        {
+            insert_at_begin(value);
+            return;
+        }
+
+        Node *newNode = new Node();
+        newNode->prev = tail;
+        tail->next = newNode;
+        tail = newNode;
+        tail->next = head;
+        head->prev = tail;
+        sizeOfLL++;
+    }
+    void insert_at_position(int value, int position)
+    {
+        if (
+            (position <= 0) &&
+            (position > sizeOfLL + 1))
+        {
+            cout << "Invalid position value. you can give position value between 1 - " << sizeOfLL + 1 << ". as the size of LL is " << sizeOfLL << endl;
+            return;
+        }
+
+        Node *temp = head;
+        while ((position - 2) > 0 && (temp->next != NULL))
+        {
+            temp = temp->next;
+            position--;
+        }
+        Node *newNode = new Node();
+        newNode->data = value;
+
+        newNode->prev = temp;
+        newNode->next = temp->next;
+        temp->next = newNode;
+        sizeOfLL++;
+    }
 };
 
 int main()
