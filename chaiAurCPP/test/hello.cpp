1cf7acd92bad9c4b9d36eebaf58d40aa6cb44b5c
kishanghosh090 <kishanghosh090@gmail.com>
2025-09-23 01:07:43 +0530

    add cpp

--- chaiAurCPP/test/hello.cpp
+++ chaiAurCPP/test/hello.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <cmath>
 namespace myChai
 {
     void print(char *arr)
@@ -10,9 +10,44 @@ namespace myChai
 
 int main()
 {
-    std::cout << "hello from chai\n";
-    myChai::print("kishan");
-    return 0;
+    int x = 10;
+    if (x < 0)
+    {
+        return false;
+    }
+
+    if (x < 100)
+    {
+        if (x / 11 == x % 10)
+        {
+            std::cout << true;
+        }
+        else
+        {
+            std::cout << false;
+        }
+    }
+
+    int temp = x;
+    int palNum = 0;
+    int i = 0;
+    while (temp > 0)
+    {
+        int r = temp % 10;
+        std::cout << r << std::endl;
+        palNum = (pow(10, i) * r) + palNum;
+        temp = temp / 10;
+        i++;
+    }
+    // std::cout << palNum << "  " << x;
+    if (x == palNum)
+    {
+        std::cout << true;
+    }
+    else
+    {
+        std::cout << false;
+    }
 }
 
 int Main()
