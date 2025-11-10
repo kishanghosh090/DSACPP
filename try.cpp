2d799a7827625dd6825148fff77aa50fbbd2e3c6
kishanghosh090 <kishanghosh090@gmail.com>
2025-11-11 00:33:04 +0530

    add try.cpp

--- try.cpp
+++ try.cpp
@@ -1,34 +1,16 @@
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
 
 int main()
 {
-    vector<int> nums = {2, 5, 2, 4, 4};
-    int maxi = nums[0];
-    for (int i : nums)
-    {
-        if (i > maxi)
-        {
-            maxi = i;
-        }
-    }
-    cout << nums[0];
+    // int a = 12;
+    // string b = to_string(a);
+    // cout << b;
 
-    int *arr = new int[maxi]();
-
-    for (int i : nums)
-    {
-        arr[i] += 1;
-    }
-
-    for (int i = 0; i <= maxi; i++)
-    {
-        if (arr[i] == 1)
-        {
-            cout << i;
-            break;
-        }
-    }
+    int n = 5;
+    int a = n & 1;
+    cout << a;
     return 0;
 }
